164de735770c63555417c153f105b01b77b6d319
raczr <raczrichardrobert@gmail.com>
2026-03-29 18:33:50 +0200

    Fajlbeolvasas

--- projekt/Program.cs
+++ projekt/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,18 @@ namespace projekt
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static List<Account> szamlak = new List<Account>();
+
+        static void Main()
         {
+            StreamReader sr = new StreamReader("szamlak.txt");
+            while (!sr.EndOfStream)
+            {
+                string sor = sr.ReadLine();
+                string[] adatok = sor.Split(';');
+                szamlak.Add(new Account(adatok[0], adatok[1], double.Parse(adatok[2])));
+            }
+            sr.Close();
         }
     }
 }
