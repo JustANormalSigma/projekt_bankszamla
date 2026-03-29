068e2ee16009600da496de9e638ea5ecac32ed0e
raczr <raczrichardrobert@gmail.com>
2026-03-29 17:09:10 +0200

    szamla_alap

--- projekt/szamla.cs
+++ projekt/szamla.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace projekt
+class Account
 {
-    internal class szamla
+    private string szamlaszam;
+    private string nev;
+    private double egyenleg;
+    private double hitelkeret;
+    private double nyitoEgyenleg;
+    private List<string> naplo = new List<string>();
+
+    public Account(string szamlaszam, string nev, double egyenleg)
     {
+        this.szamlaszam = szamlaszam;
+        this.nev = nev;
+        this.egyenleg = egyenleg;
+        this.nyitoEgyenleg = egyenleg;
+        this.hitelkeret = 0;
     }
-}
+
+    public string GetSzamlaszam()
+    {
+        return szamlaszam;
+    }
+
